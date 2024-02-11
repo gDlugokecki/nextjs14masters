69c6c756050956d7dc196a62cba1603e4f2e6de1
Grzegorz Dlugokecki <grzesiek.dlugokecki@gmail.com>
2024-02-11 17:36:52 +0100

    Add data-testid to productsList

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -8,7 +8,10 @@ type ProductListProps = {
 
 export const ProductList = ({ products }: ProductListProps) => {
 	return (
-		<ul className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+		<ul
+			className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+			data-testid="products-list"
+		>
 			{products.map((product) => {
 				return <ProductListItem key={product.id} product={product} />;
 			})}
